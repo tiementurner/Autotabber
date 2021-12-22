02ad728e199866cbba6f2590ae51eb415de935ac
tiemen <tiemenr@gmail.com>
2021-12-22 17:28:39 +0100

    simple fft

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from ctypes import *
 import pyaudio
 import wave
 import sys
+import scipy.io.wavfile as wavfile
+import numpy as np
+import pylab as pl
 
 ERROR_HANDLER_FUNC =  CFUNCTYPE(None, c_char_p, c_int, c_char_p, c_int, c_char_p)
 def	py_error_handler(filename, line, function, err, fmt):
@@ -15,8 +18,16 @@ if len(sys.argv) < 2:
 	print("Give 1 single argument u damn idiot!")
 	sys.exit(-1)
 
-wf = wave.open(sys.argv[1], 'rb')
+rate, data = wavfile.read(sys.argv[1])
 
+p = 20*np.log10(np.abs(np.fft.rfft(data[:2048])))
+f = np.linspace(0, rate/2.0, len(p))
+pl.plot(f,p)
+pl.xlabel("Freq")
+pl.ylabel("Power(dB)")
+pl.show()
+
+"""
 p = pyaudio.PyAudio()
 
 stream = p.open(format = p.get_format_from_width(wf.getsampwidth()),
@@ -34,3 +45,4 @@ stream.stop_stream()
 stream.close()
 
 p.terminate()
+"""
