c94d8a9af2aa321404a22eca3a760f133c1ea204
Tiemen reijer Blankert <tblanker@f0r3s18.codam.nl>
2022-12-07 20:49:05 +0100

    started over in C++, added huge fftw lib

---
+++ inc/includes.hpp
@@ -0,0 +1,13 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <cstdint>
+
+#include <fftw.h>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::fstream;
+using std::string;
